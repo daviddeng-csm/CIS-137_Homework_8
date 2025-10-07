820d057c7884557ac98701f7c9df02ee647b6ee9
David Deng <daviddeng@my.smccd.edu>
2025-10-07 11:42:24 -0700

    Initial Commit

---
+++ Homework8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework8
+//
+//  Created by David Deng on 10/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework8Tests/Homework8Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Homework8Tests.swift
+//  Homework8Tests
+//
+//  Created by David Deng on 10/7/25.
+//
+
+import Testing
+@testable import Homework8
+
+struct Homework8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
